ae857d97c109586e974b35c91915ca88f36aee7e
matthiasjk5 <matthias.jk5@gmail.com>
2019-10-29 06:51:38 -0400

    adding board and logic

--- TicTacToeWithWindowsForms/CalculateMark.cs
+++ TicTacToeWithWindowsForms/CalculateMark.cs
@@ -8,6 +8,37 @@ namespace TicTacToeWithWindowsForms
 {
     class CalculateMark
     {
-        public static int whoseTurn = 0; // User: 0, Computer: 1
+        // return 0: user wins. return 1: computer wins
+        public int WhoWin()
+        {
+            if (Board.userBoard[0, 0] == 1 && Board.userBoard[0, 1] == 1 && Board.userBoard[0, 2] == 1)
+            {
+                return 0;
+            }
+            else if (Board.userBoard[1, 0] == 1 && Board.userBoard[1, 1] == 1 && Board.userBoard[1, 2] == 1)
+            {
+                return 0;
+            }
+            else if (Board.userBoard[2, 0] == 1 && Board.userBoard[2, 1] == 1 && Board.userBoard[2, 2] == 1)
+            {
+                return 0;
+            }
+            else if (Board.userBoard[0, 0] == 1 && Board.userBoard[1, 0] == 1 && Board.userBoard[2, 0] == 1)
+            {
+                return 0;
+            }
+            else if (Board.userBoard[0, 1] == 1 && Board.userBoard[1, 1] == 1 && Board.userBoard[2, 1] == 1)
+            {
+                return 0;
+            }
+            else if (Board.userBoard[0, 2] == 1 && Board.userBoard[1, 2] == 1 && Board.userBoard[2, 2] == 1)
+            {
+                return 0;
+            }
+            else if (Board.userBoard[0, 0] == 1 && Board.userBoard[1, 1] == 1 && Board.userBoard[2, 2] == 1)
+            {
+                return 0;
+            }
+        }
     }
 }
--- TicTacToeWithWindowsForms/Form1.cs
+++ TicTacToeWithWindowsForms/Form1.cs
@@ -24,20 +24,22 @@ namespace TicTacToeWithWindowsForms
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (CalculateMark.whoseTurn %2 == 0)
+            if (Board.whoseTurn %2 == 0)
             {
                 button1.Text = "X";
                 button1.Enabled = false;
-                CalculateMark.whoseTurn++;
+                Board.whoseTurn++;
                 Board.userBoard[0,0] = 1;
             }
             else
             {
                 button1.Text = "0";
                 button1.Enabled = false;
-                CalculateMark.whoseTurn++;
-                Board.comBoard[0, 0] = 0;
+                Board.whoseTurn++;
+                Board.comBoard[0, 0] = 1;
             }
+
+            checkWin();
         }
     }
 }
